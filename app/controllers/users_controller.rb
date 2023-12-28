bc862825a1aede4890c29b180cf9d046aa13d54f
tomdan-ai <tomudoh258@gmail.com>
2023-12-28 21:44:16 +0100

    Write CRUD API for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
-end
+    before_action :set_user, only: [:show, :update, :destroy]
+  
+    # GET /users
+    def index
+      @users = User.all
+      render json: @users
+    end
+  
+    # GET /users/1
+    def show
+      render json: @user
+    end
+  
+    # POST /users
+    def create
+      @user = User.new(user_params)
+  
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /users/1
+    def update
+      if @user.update(user_params)
+        render json: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /users/1
+    def destroy
+      @user.destroy
+      render json: { message: 'User deleted successfully' }
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :profile_picture, :bio)
+    end
+  end
+  
