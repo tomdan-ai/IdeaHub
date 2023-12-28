f64bcbcd5a57669f433556ae85a95ae13e2841f0
tomdan-ai <tomudoh258@gmail.com>
2023-12-28 21:08:33 +0100

    Write all Database Relatioships

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :article_categories, dependent: :destroy
+  has_many :categories, through: :article_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
-end
+    has_many :article_categories, dependent: :destroy
+    has_many :articles, through: :article_categories
+  end
+  
