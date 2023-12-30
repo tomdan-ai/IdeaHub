b97c035659bb64ce3809de1d49f1693d50855c2c
tomdan-ai <tomudoh258@gmail.com>
2023-12-30 08:19:45 +0100

    Rewriete Models to integrate Reactions

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :article_categories, dependent: :destroy
   has_many :categories, through: :article_categories
+  has_many :reactions, as: :reactable, dependent: :destroy
 end
