d38884f6224908426fa97d404e7376bb4b574436
tomdan-ai <tomudoh258@gmail.com>
2023-12-30 08:19:01 +0100

    Write CRUD API for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,51 @@
 class CategoriesController < ApplicationController
-end
+    before_action :set_category, only: [:show, :update, :destroy]
+  
+    # GET /categories
+    def index
+      @categories = Category.all
+      render json: @categories
+    end
+  
+    # GET /categories/1
+    def show
+      render json: @category
+    end
+  
+    # POST /categories
+    def create
+      @category = Category.new(category_params)
+  
+      if @category.save
+        render json: @category, status: :created
+      else
+        render json: @category.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /categories/1
+    def update
+      if @category.update(category_params)
+        render json: @category
+      else
+        render json: @category.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /categories/1
+    def destroy
+      @category.destroy
+      render json: { message: 'Category deleted successfully' }
+    end
+  
+    private
+  
+    def set_category
+      @category = Category.find(params[:id])
+    end
+  
+    def category_params
+      params.require(:category).permit(:name)
+    end
+  end
+  
