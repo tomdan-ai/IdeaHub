e093640a8c79437adf6b6e2f7ec6e9d3564ca810
tomdan-ai <tomudoh258@gmail.com>
2024-01-06 16:15:16 +0100

    Write authorizations codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
+    before_action :authenticate_user!
+
+    private
+    def authenticate_user!
+        token = request.headers['Authorization']&.split(' ')&.last
+        decode_token = JwtService.decode(token)
+
+        unless decode_token
+        render json: { error: 'Unauthorized' }, status: unauthorized
+        end
+    end
 end
