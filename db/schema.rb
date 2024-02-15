7b54bcf9189d433aa5167bdba014aad83c8cd967
tomdan-ai <tomudoh258@gmail.com>
2024-02-15 15:04:48 +0100

    Set up email verification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_20_085925) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_140318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_085925) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "email_verified"
   end
 
   add_foreign_key "articles", "users"
